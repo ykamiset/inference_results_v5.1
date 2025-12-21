e9ccad4d681ac32678230a488cd690ef6c8b21fa
root <root@smci355-2b805-c2-4.aus-b200.dcgpu>
2025-12-21 15:16:30 -0600

    trllm fix

--- closed/NVIDIA/docker/release/install_trt_llm.sh
+++ closed/NVIDIA/docker/release/install_trt_llm.sh
@@ -1,4 +1,5 @@
-git clone ${TRTLLM_URL} /opt/trtllm
-cd /opt/trtllm
-git checkout ${TRTLLM_HASH}
-python scripts/build_wheel.py --use_ccache --benchmarks -a="100-real" --install
+# git clone ${TRTLLM_URL} /opt/trtllm
+# cd /opt/trtllm
+# git checkout ${TRTLLM_HASH}
+# python scripts/build_wheel.py --use_ccache --benchmarks -a="100-real" --install
+echo "Installing TRTLLM... skipped"
